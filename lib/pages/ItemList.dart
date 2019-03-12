7732270208a07b35da6efa1b21413d1824a62d28
RubyWoo <46455916+RubyWoo@users.noreply.github.com>
2019-03-12 13:21:49 -0600

    se agrego prueba json

--- lib/pages/ItemList.dart
+++ lib/pages/ItemList.dart
@@ -1,21 +1,45 @@
-import 'package:app_editesp/BackW.dart';
 import 'package:app_editesp/BackWGeneral.dart';
 import 'package:flutter/material.dart';
 import 'package:app_editesp/ListModel.dart';
 import '../product_manager.dart';
-import 'package:android_intent/android_intent.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:io';
+import 'dart:convert' as JSON;
+
+String myJSONProducts = '{"title":"TAREA A","description":"Descripcion General"}';
 
 List<String> prod = ["TAREA A","TAREA B","TAREA C","TAREA D", "TAREA E"];
 
 
 class ItemList extends StatelessWidget {
 
+  Map<String,dynamic> jsonMapProducts = JSON.jsonDecode(myJSONProducts);
+
+  void _printJSON(){
+    print(jsonMapProducts);
+    print(jsonMapProducts["title"]);
+    print(jsonMapProducts["description"]);
+  }
+
 
   _openMap() async {
     // Android
-    const url = 'geo:19.404225,-99.154043';
+    var url = 'geo:0,0?q=Parque+Delta';
+    if (Platform.isIOS) {
+      // iOS
+      url = 'http://maps.apple.com/?ll=19.404225,-99.154043';
+    }
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+
+  _openMap2() async {
+    // Android
+    const url = 'geo:0,0?q=Parque+Delta';
     if (await canLaunch(url)) {
       await launch(url);
     } else {
@@ -45,9 +69,10 @@ class ItemList extends StatelessWidget {
       floatingActionButton:  FloatingActionButton(
         backgroundColor: Colors.greenAccent,
         child: Icon(
-          Icons.map,
+          Icons.adb,
           color: Colors.black,),
         onPressed: (){
+         // _printJSON();
           _openMap();
 
         },
