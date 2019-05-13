d394f613b5c1aa541f0f3ab0a1198eb6a0dd7a7c
kato <kvaldez@centraldealarmas.com.mx>
2019-05-13 17:33:20 -0500

    Pruebas de publiucacion

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowMaterialGrid: true,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
           brightness: Brightness.light,
           primarySwatch: Colors.blue,
